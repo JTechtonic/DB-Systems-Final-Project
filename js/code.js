d7965d0ec1e2ba551bf656c8e1847a613d874e47
Owen Burow <Owenburow@gmail.com>
2025-03-24 11:49:49 -0400

    Bring over the base I had in POOSED

---
+++ js/code.js
@@ -0,0 +1,163 @@
+const urlBase = '';
+const extension = 'php';
+
+var userId = 0;
+let firstName = "";
+let lastName = "";
+
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+
+  //gets the username and password from the input tag on the .html that calls it
+	let login = document.getElementById("loginUsername").value;
+	let password = document.getElementById("loginPassword").value;
+//	var hash = md5( password );
+
+	document.getElementById("loginResult").innerHTML = "";
+
+  //creates javascript struct
+  //this is a struct
+	let tmp = {login:login,password:password};
+  //	var tmp = {login:login,password:hash};
+  // converts the struct to a json blob
+	let jsonPayload = JSON.stringify( tmp );
+
+  //this picks the php file
+	let url = urlBase + '/Login.' + extension;
+
+  //this is what actually creates the post request (AJAX)
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+    //this defines the reponse processing functions
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+
+				if( userId < 1 )
+				{
+					document.getElementById("loginResult").innerHTML = "<span>User/Password combination incorrect</span>";
+					return;
+				}
+
+				saveCookie();
+
+				window.location.href = "dashboard.html";
+			}
+		};
+    //This sends the message to the url (Login.php)
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
+
+}
+
+function doRegister()
+{
+	console.log("doRegister working");
+	let firstName = document.getElementById("registerFirstName").value;
+	let lastName = document.getElementById("registerLastName").value;
+	let login = document.getElementById("registerUsername").value;
+	let password = document.getElementById("registerPassword").value;
+//	var hash = md5( password );
+
+	document.getElementById("registerResult").innerHTML = "";
+
+	let tmp = {FirstName:firstName,LastName:lastName,login:login,password:password};
+
+//	var tmp = {login:login,password:hash};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/AddUser.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+				userId = jsonObject.id;
+
+				if( jsonObject.error !== "" )
+				{
+					document.getElementById("registerResult").innerHTML = jsonObject.error;
+					return;
+				}
+
+				window.alert("Successful Registeration");
+
+				saveCookie();
+
+				window.location.href = "index.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("registerResult").innerHTML = err.message;
+		window.alert(err.message);
+	}
+
+}
+
+function saveCookie()
+{
+	let minutes = 20;
+	let date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	let data = document.cookie;
+	console.log("Cookies:", data);
+
+	let splits = data.split(",");
+	for(var i = 0; i < splits.length; i++)
+	{
+	    let thisOne = splits[i].trim();
+		let tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+	console.log("User ID found:", userId);
+	return userId;
+}
+
+}
